d1b8fa28640804468cc5bdf85e3c24de08014574
sgednethgirao <edneth@sheetgo.com>
2022-12-28 12:52:54 +0100

    adding message broadcast

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ const messageInput = document.getElementById('message')
 const handleInput = document.getElementById('handle')
 const sendBtn = document.getElementById('send')
 const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
 
 // Emit events
 sendBtn.addEventListener('click', () => {
@@ -15,7 +16,16 @@ sendBtn.addEventListener('click', () => {
     })
 })
 
+messageInput.addEventListener('keypress', () => {
+    socket.emit('typing', handle.value)
+})
+
 // Listen for events
 socket.on('chat', (data) => {
+    feedback.innerHTML = ''
     output.innerHTML += '<p><strong>' + data.handle + ':</strong> ' + data.message + '</p>'
+})
+
+socket.on('typing', (data) => {
+    feedback.innerHTML = '<p><em> '+ data +' is typing a message...</em></p>'
 })
