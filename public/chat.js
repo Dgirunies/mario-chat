0571a3d0653fb21f87c6dd55fd75e09c14c4975d
sgednethgirao <edneth@sheetgo.com>
2022-12-28 12:45:43 +0100

    adding message exchange

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,21 @@
 // Make connection
 const socket = io.connect('http://localhost:4000')
+
+// Query DOM
+const messageInput = document.getElementById('message')
+const handleInput = document.getElementById('handle')
+const sendBtn = document.getElementById('send')
+const output = document.getElementById('output')
+
+// Emit events
+sendBtn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: messageInput.value,
+        handle: handleInput.value
+    })
+})
+
+// Listen for events
+socket.on('chat', (data) => {
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong> ' + data.message + '</p>'
+})
